d0e26843694b24c2ea4e9b22fd44079accd44261
Ovelar <novelar@hexacta.com>
2017-12-17 17:09:06 -0300

    Cambios en el ruteo de Usuario

--- book/urls.py
+++ book/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import url
-
 from book import views
 
 urlpatterns = [
     url(r'^$', views.book_list, name='book_list'),
     url(r'^new$', views.book_create, name='book_new'),
-    url(r'^edit/(?P<pk>\d+)$',
-        views.book_update, name='book_edit'),
-    url(r'^delete/(?P<pk>\d+)$',
-        views.book_delete, name='book_delete'),
+    url(r'^edit/(?P<pk>\d+)$', views.book_update, name='book_edit'),
+    url(r'^delete/(?P<pk>\d+)$', views.book_delete, name='book_delete'),
 ]
--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,8 +3,8 @@ from mainApp.views import UsuarioListView, UsuarioCreateView, UsuarioUpdateView,
 from mainApp import views
 
 urlpatterns = [
-    url(r'^/$', UsuarioListView.as_view(), name='usuarios_list'),
-    url(r'^/crear$', UsuarioCreateView.as_view(), name='usuario_create'),
-    url(r'^/editar/(?P<pk>\d+)$', UsuarioUpdateView.as_view(), name='usuario_update'),
-    url(r'^/borrar/(?P<pk>\d+)$', UsuarioDeleteView.as_view(), name='usuario_delete'),    
+    url(r'^$', UsuarioListView.as_view(), name='usuarios_list'),
+    url(r'^new$', UsuarioCreateView.as_view(), name='usuario_create'),
+    url(r'^edit/(?P<pk>\d+)$', UsuarioUpdateView.as_view(), name='usuario_update'),
+    url(r'^delete/(?P<pk>\d+)$', UsuarioDeleteView.as_view(), name='usuario_delete'),    
 ]
